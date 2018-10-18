41778c46642893c9e4031208cfc11c029d3039c0
Alex Roh <alex.roh@nyu.edu>
2018-10-18 16:27:31 -0700

    Basic UI set up, buttons wired

--- app/src/main/java/com/digitalidllc/alex_roh/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/digitalidllc/alex_roh/guessthecelebrity/MainActivity.java
@@ -1,9 +1,36 @@
 package com.digitalidllc.alex_roh.guessthecelebrity;
 
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    public class dataDownloader extends AsyncTask<String, Void, Pair<String,String>>{
+        @Override
+        protected Pair<String, String> doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+                return null;
+            }
+            return null;
+        }
+    }
+
+    public void selectAnswer(View view){
+        Log.i("Button","select answer");
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
