2b7bccd4d54161181472930f050fc07eef7647bc
Alex Roh <alex.roh@nyu.edu>
2018-10-18 16:45:13 -0700

    AsyncTask set up

--- app/src/main/java/com/digitalidllc/alex_roh/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/digitalidllc/alex_roh/guessthecelebrity/MainActivity.java
@@ -9,20 +9,38 @@ import android.util.Pair;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    public class dataDownloader extends AsyncTask<String, Void, Pair<String,String>>{
+    private ArrayList<Pair<String,String>> celebrities;
+
+    public class dataDownloader extends AsyncTask<String, Void, String>{
         @Override
-        protected Pair<String, String> doInBackground(String... urls) {
+        protected String doInBackground(String... urls) {
             try {
                 URL url = new URL(urls[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                String result="";
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+
             }
             catch(Exception e){
                 e.printStackTrace();
-                return null;
+                return "Failed";
             }
-            return null;
         }
     }
 
@@ -36,5 +54,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dataDownloader downloader = new dataDownloader();
+        String result="";
+        try{
+            result=downloader.execute("http://www.posh24.se/kandisar").get();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        Log.i("Result: ", result);
     }
 }
